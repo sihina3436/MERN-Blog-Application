ad76895201ed423129505ca4a1dfe1841402c45e
sihina3436 <nimnadasihina@gmail.com>
2025-02-04 21:17:54 +0530

    Fix tailwind css bug in code

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,11 +3,10 @@ import {Link} from 'react-router-dom'
 import React from 'react'
 import { BsFacebook, BsInstagram, BsGithub, BsDribbble } from "react-icons/bs";
 
-
 const FooterCom = () => {
   return (
    <Footer container className='border border-t-8 border-teal-500'>
-        <div className='w-full max-w-7xl max-auto'>
+        <div className='w-full max-w-7xl mx-auto'>
             <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
                 <div className='mt-5 '>
                      <Link to="/" className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'>
