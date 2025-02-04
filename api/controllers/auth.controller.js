f993797be7fa45fc2aee7e1b21565b8900f33069
sihina3436 <nimnadasihina@gmail.com>
2025-02-04 23:31:39 +0530

    Create SignIn route with JWT

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   try {
@@ -30,3 +31,32 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signin = async (req,res,next) => {
+  const {email, password} = req.body;
+
+  if(!email || !password || email === '' || password === ''){
+    next(errorHandler(400,'All fields are required..'));
+  }
+
+  try {
+    const ValidUser = await User.findOne({email});
+    if(!ValidUser){
+      return next(errorHandler(400,'User not found'));
+    }
+    const validPassword = bcryptjs.compareSync(password,ValidUser.password);
+    if(!validPassword){
+      return next(errorHandler(400,'invalid password'));
+    }
+    const token = jwt.sign({id:ValidUser._id,}, process.env.JWT_SECRET);
+
+    const {password: pass, ...rest} = ValidUser._doc; // destructuring assignment to exclude the password field from the user object before sending the response
+
+    res.status(200).cookie('access_token', token, {
+      httpOnly: true
+    }).json(rest);
+  } catch (error) {
+    next(error);
+  }
+
+}
